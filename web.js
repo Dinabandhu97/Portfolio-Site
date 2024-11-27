a93ff944071ea5b88b1cff76fe52cf49b422f173
Dinabandhu Rout <drout1734@gmail.com>
2024-11-27 18:04:19 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,50 @@
+document.querySelector("header .nav_bar .outer").addEventListener("click",hi5);
+    function hi5(){
+        // document.querySelector("main").classList.toggle("col");
+        // document.querySelector("footer").classList.toggle("col1");
+        // document.querySelector(".inner").classList.toggle("inner1");
+        //alert("hi");
+        document.querySelector(".inner").classList.toggle("ou");
+        // document.querySelector("body").classList.toggle("ou1");
+        document.querySelector("main").classList.toggle("ou1");
+        document.querySelector("main").classList.toggle("d");
+        document.querySelector("main").classList.toggle("n");
+        //document.querySelector("footer").classList.toggle("ou");
+        document.querySelector("footer .ic").classList.toggle("g2");
+        document.querySelector(".nav_bar").classList.toggle("ou3");
+        document.querySelector("header .nav_bar").classList.toggle("g1");
+    }
+
+
+    document.querySelector(".nav_bar .i").addEventListener("click",hi1);
+    function hi1(){
+        // document.querySelector("main").classList.toggle("col");
+        // document.querySelector("footer").classList.toggle("col1");
+        // document.querySelector(".inner").classList.toggle("inner1");
+        //alert("hi");
+        document.querySelector(".nav_bar .menu").classList.add("h");
+        alert("D M");
+        // document.querySelector("body").classList.toggle("ou1");
+        // document.querySelector("main").classList.toggle("ou1");
+        // document.querySelector("footer").classList.toggle("ou2");
+        // document.querySelector(".nav_bar").classList.toggle("ou3");
+    }
+
+
+    document.querySelector(".cross").addEventListener("click",hi);
+    function hi(){
+        
+        document.querySelector(".we").classList.add("k");
+        document.querySelector("header").classList.add("k1");
+        document.querySelector("main").classList.add("k2");
+        document.querySelector("footer").classList.add("k3");
+    }
+
+    document.querySelector(".we .button").addEventListener("click",hi);
+    function hi(){
+        
+        document.querySelector(".we").classList.add("k");
+        document.querySelector("header").classList.add("k1");
+        document.querySelector("main").classList.add("k2");
+        document.querySelector("footer").classList.add("k3");
+    }
---
+++ web1.js
@@ -0,0 +1,16 @@
+document.querySelector(".nav_bar .outer").addEventListener("click",hi5);
+    function hi5(){
+        // document.querySelector("main").classList.toggle("col");
+        // document.querySelector("footer").classList.toggle("col1");
+        // document.querySelector(".inner").classList.toggle("inner1");
+        //alert("hi");
+        document.querySelector(".inner").classList.toggle("ou");
+        // document.querySelector("body").classList.toggle("ou1");
+
+        //document.querySelector("footer").classList.toggle("ou");
+
+        document.querySelector(".nav_bar").classList.toggle("ou3");
+        document.querySelector(".nav_bar").classList.toggle("g1");
+        document.querySelector("body").classList.toggle("g3");
+        document.querySelector(".h").classList.toggle("g4");
+    }
